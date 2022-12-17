f3351051ce38825144ff8a6d911c0e09af044277
Mayank Gaur <68467767+gaurmayank05@users.noreply.github.com>
2022-12-18 01:06:10 +0530

    Add files via upload
    Program a code for leap year using function.

---
+++ Leap_year/leap_year.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+
+void leap_year(){
+    int year;
+    char opt;
+
+    //clrscr();
+    L1:
+    printf("Enter year: ");
+    scanf("%d",&year);
+    if(year % 400 == 0){
+        printf("%d is a leap year.\n",year);
+    }
+    else if(year % 100==0){
+        printf("%d is not a leap year.\n",year);
+    }
+    else if(year % 4 ==0){
+        printf("%d is a leap year.\n",year);
+    }
+    else{
+        printf("%d is not a leap year.\n",year);
+    }
+    printf("Whether you further want check?        (y/n)\n");
+    fflush(stdin);    
+    scanf("%c",&opt);
+    if (opt == 'y'){
+        goto L1;
+    }
+}
+
+void main(){
+    leap_year();
+
+}
