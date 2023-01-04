ec37131ae56c2d875e985303b26844ec6355575c
Mayank Gaur <68467767+gaurmayank05@users.noreply.github.com>
2023-01-04 23:05:09 +0530

    Update Average.c

--- Average.c
+++ Average.c
@@ -3,7 +3,7 @@
 #include<stdio.h>
 #include<math.h>
 
-/*
+
 void main(){
     int m1,m2,m3,m4,m5;
     int sum;
@@ -12,29 +12,4 @@ void main(){
     sum = m1+m2+m3+m4+m5;
     printf("Obtained Marks: %d \n",sum);
     printf("percentage: %0.2f ",sum/5.0);
-}
-*/
-
-// Write the same programe using function and goto
-
-void main(){
-    void marks();
-    marks();
-}
-
-void marks(){
-    int m1,m2,m3,m4,m5,sum;
-    char ch;
-    marks:
-    printf("Enter marks:\n");
-    scanf("%d %d %d %d %d",&m1,&m2,&m3,&m4,&m5);
-    sum=m1+m2+m3+m4+m5;
-    printf("Obtained marks: %d \n",sum);
-    printf("percentage: %0.2f\n",sum/5.0);
-    printf("Whether you again want to calculate:              (y/n)\n");
-    fflush(stdin);
-    scanf("%c",&ch);
-    if (ch=='y'){
-        goto marks;
-    }
 }
